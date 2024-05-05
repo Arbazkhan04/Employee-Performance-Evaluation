5c50ab7bd981350c6bd523004a1a2d5b161d47ab
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2024-05-05 12:41:02 +0500

    Home PAge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from './Components';
+import { Home } from './Pages';
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Routes>
+      <Route path='/' element={<Home/>}/>  
+        
       </Routes>
       <Footer/>
     </Router>
