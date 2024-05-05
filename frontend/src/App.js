451fcbbf9f3d50b4d94665e260f4bf3f75be0a5a
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2024-05-05 14:33:05 +0500

    Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from './Components';
 import { Employee, Home,Grouper } from './Pages';
 import Supervisor from './Components/Supervisor/Supervisor';
+import Evaluate from './Pages/Evaluation/Evaluate';
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
       <Route path='/Employee' element={<Employee/>}/>  
       <Route path='/Groups' element={<Grouper/>}/>      
       <Route path='/Supervisor' element={<Supervisor/>}/>      
+      <Route path='/Evaluation' element={<Evaluate/>}/>      
+      
       </Routes>
       <Footer/>
     </Router>
