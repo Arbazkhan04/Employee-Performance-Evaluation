0d18dd4c16e869fe5530445eef207b14ce12bac9
Abdur Rehman Kazim <abdurrehmankazim68@gmail.com>
2024-05-06 10:46:33 +0500

    add

--- app/routes/groupRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { getAllGroups, makeGroup, addEmployeeToGroup } = require('../controller/groupController')
-
-const router = express.Router()
-
-router.route('/').get(getAllGroups).post(makeGroup)
-router.route('/:id').post(addEmployeeToGroup)
-
-module.exports = router
